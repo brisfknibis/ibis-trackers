f8c2fa488d8f0b73ec94b9ca42c7e584cd039794
Matthew Neal <emily.pereira.bris@gmail.com>
2025-04-19 12:10:52 +1000

    Added Firmware Flasher

---
+++ Firmware/firmwareFlasher.ps1
@@ -0,0 +1,23 @@
+# Path to your firmware file
+$firmwarePath = "D:\STL Files\Bris Trackers\Firmware\SellSPI30MinSensCal.uf2"
+
+# Target volume label of the NiceNano
+$targetLabel = "NICENANO"
+
+# Infinite loop
+while ($true) {
+    # Get all drives with label NICENANO
+    $drives = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.VolumeName -eq $targetLabel }
+
+    foreach ($drive in $drives) {
+        $targetPath = Join-Path $drive.DeviceID (Split-Path $firmwarePath -Leaf)
+
+        if (-Not (Test-Path $targetPath)) {
+            Write-Host "[$(Get-Date -f 'HH:mm:ss')] Found $($drive.DeviceID), copying firmware..."
+            Copy-Item -Path $firmwarePath -Destination $targetPath -Force
+            Write-Host "[$(Get-Date -f 'HH:mm:ss')] Copy complete."
+        }
+    }
+
+    Start-Sleep -Seconds 2
+}
